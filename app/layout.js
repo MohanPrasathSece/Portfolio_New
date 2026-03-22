8d1dae0226c4651c6340ccbe2cbc551fad8c128c
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2026-03-22 17:21:04 +0530

    Add Google Analytics tag to layout

--- app/layout.js
+++ app/layout.js
@@ -71,8 +71,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en-IN">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-0XFJXRK25R"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-0XFJXRK25R');
+		</script>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
         <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
